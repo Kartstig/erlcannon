6a80621d2aedf7b8081363ca7e5320aec23f8387
Herman Singh <kartstig@gmail.com>
2018-04-02 08:49:27 -0400

    Refactor worker to use gen_server state

--- src/worker.erl
+++ src/worker.erl
@@ -1,32 +1,58 @@
 -module(worker).
 -behaviour(gen_server).
 
--export([start_link/0, stop/0]).
--export([code_change/3, init/1, handle_call/3, handle_info/2, terminate/2]).
+-export([code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1, start_link/1, terminate/2]).
+-export([results/1]).
+
+-include("worker_state.hrl").
 
 
-start_link() ->
-  gen_server:start_link(?MODULE, [], []).
+% API
+-spec results(term()) -> #worker_state{}.
+results(Name) ->
+  gen_server:call(Name, {results}).
+
+
+% gen_server
+
+start_link(Name) ->
+  gen_server:start_link({local, Name}, ?MODULE, [], []).
 
 init([]) ->
-  {ok, []}.
+  {ok, #worker_state{}}.
 
-handle_call({perform, Uri, _Origin}, _From, Res) ->
-  {Time, {ok, {{_Version, _200, _ReasonPhrase}, _Headers, _Body}}} = timer:tc(httpc, request, [Uri]),
-  {reply, {done, Time}, Res};
-handle_call(terminate, _From, Res) ->
-  {stop, normal, ok, Res}.
+handle_cast({perform, Uri}, State) ->
+  case timer:tc(httpc, request, [Uri]) of
+    {Time, {ok, {{_Version, _200, _ReasonPhrase}, _Headers, _Body}}} ->
+      NewState = State#worker_state{
+        requests=(State#worker_state.requests + 1),
+        time=(State#worker_state.time + Time),
+        resp_200=(State#worker_state.resp_200 + 1)
+        },
+      {noreply, NewState};
+    {Time, {error, {_Reason,_}}} ->
+      NewState = State#worker_state{
+        requests=(State#worker_state.requests + 1),
+        time=(State#worker_state.time + Time)
+        },
+      {noreply, NewState};
+    BadResult ->
+      io:format("Error:~n~p~n", [BadResult]),
+      {noreply, State}
+  end.
 
-handle_info(Msg, Res) ->
+handle_call(results, _From, State) ->
+  {reply, State, State};
+handle_call(terminate, _From, State) ->
+  {stop, normal, ok, State}.
+
+handle_info(Msg, State) ->
   io:format("Unexpected message: ~p~n",[Msg]),
-  {noreply, Res}.
+  {noreply, State}.
 
-terminate(normal, _Res) ->
+terminate(normal, _State) ->
   io:format("Worker ~w terminating~n", [self()]),
   ok.
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
-
-stop() ->
-  ok.
---
+++ src/worker_state.hrl
@@ -0,0 +1,5 @@
+-record(worker_state, {
+        requests = 0  :: integer(),
+        time = 0      :: float(),
+        resp_200 = 0  :: integer()
+        }).
